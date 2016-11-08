9bb45b9fcb884a9c9c55e63bd8860d7f6d0774ad
Ivan <ivan.matas2@gmail.com>
2016-11-08 16:50:02 +0000

    add home page

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ var UAParser = require('ua-parser-js');
 var parser = new UAParser();
 
 app.set("view engine","ejs");
-app.get("/whoami",function(req,res) {
+app.use(express.static(__dirname + "/public"));
+
+app.get("/",function(req, res) {
+    res.render("home");
+});
+app.get("/api/whoami",function(req,res) {
     var info ={
         ipAddress: req.headers["x-forwarded-for"],
         language: req.headers["accept-language"].split(",")[0],
